b273550b66295c121db492367e4f120557282ab3
Marcin Bojko <marcin@bojko.com.pl>
2025-01-23 06:24:15 +0100

    Version 1.1.22

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName        = 'doublecmd'
-$version            = '1.1.21'
+$version            = '1.1.22'
 $url                = "https://github.com/doublecmd/doublecmd/releases/download/v$version/doublecmd-$version.i386-win32.msi"
 $url64              = "https://github.com/doublecmd/doublecmd/releases/download/v$version/doublecmd-$version.x86_64-win64.msi"
-$checksum           = 'f1d23ddcf7b2f19ad8c4e4116adb344c7dc111265d0a9493e624fa3eb4f41fe5 '
-$checksum64         = 'a7ca81c940c9037d9a6c85f61171a2ef94b70826644132a5a9ee0be8568a58ea'
+$checksum           = '96d2676acc551ca6aac967099048045d3a39b91b728b96019411925ba66437de '
+$checksum64         = 'ad666fbf7e6e4b1e99f91c454ad1b51bf6ee063bc4fbec833e50d0ceff06c767'
 $killexec           = 1
 $killexecprocess    = "doublecmd*"
 
